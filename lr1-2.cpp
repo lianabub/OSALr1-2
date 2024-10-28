de3f4dca6c88f09f8999f291eba26500bf2576a0
liana <lianabub07@gmail.com>
2024-10-28 12:38:03 +0300

    Add files via upload

---
+++ lr1-2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double z1, z2, b, a;
+    std::cout << "Введите a \n";
+    std::cin >> a;
+    std::cout << "Введите b \n";
+    std::cin >> b;
+    z1 = pow((cos(a) - cos(b)), 2) - pow((sin(a) - sin(b)), 2);
+    std::cout << "z1 = " << " ; z2 = " << z2;
+    return 0;
+}
+
